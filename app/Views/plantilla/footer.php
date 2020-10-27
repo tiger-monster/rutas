c5c61f10b1aeded7ca99d2260b0b90233b6635c2
tiger-monster <72220910+tiger-monster@users.noreply.github.com>
2020-10-27 15:15:13 -0600

    Add files via upload

--- app/Views/plantilla/footer.php
+++ app/Views/plantilla/footer.php
@@ -1,3 +1,13 @@
+<div class="card bg-dark text-white">
+  <img class="card-img" src="<?php echo base_url(); ?>/public/img/footer2.jpg" alt="Card image">
+  <div class="card-img-overlay">
+    <center><h5 style="font-size: 25pt; font-family: Times Roman;color: #9aa4fb;" class="card-title"><b>Dr.House</b></h5></center>
+    <p style="font-size: 18pt; font-family: Times Roman;color: #9aa4fb;" class="card-text"><b>Serie de T.V. estadounidense, de medicina, pero con drama y cierta manera con un toque de filosofia</b>
+    	<br></p>
+    <p style="font-size: 25pt; font-family: Times Roman;color: #9aa4fb;" class="card-text">Desarrollado por Ricardo Hernandez Ramirez</p>
+  </div>
+</div>
+
  <!-- Optional JavaScript -->
  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
  <script src="<?php echo base_url(); ?>/public/js/jquery-3.5.1.min.js"></script>
